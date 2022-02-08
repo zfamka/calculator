e3e8d285bdd347577f35161a419ae636bc316141
zfamka <alexkravchenko27@gmail.com>
2022-02-08 21:09:42 +0200

    add arc

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const main = document.querySelector('.main');
-const child = main.children;
-
-setTimeout(() => {
-    for(let i = 0; i < child.length; i++) {
-        child[i].classList.add('transitionEffect');
-    }
-  }, 100) 
