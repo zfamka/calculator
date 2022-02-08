531ce63376eeb8a73c0782cdf0382d5936899bba
zfamka <alexkravchenko27@gmail.com>
2022-02-08 21:01:46 +0200

    add layout

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('.main');
+const child = main.children;
+
+setTimeout(() => {
+    for(let i = 0; i < child.length; i++) {
+        child[i].classList.add('transitionEffect');
+    }
+  }, 100) 
