6a8b97eddda7fe92b16307827fa05aeaa3d0c58a
Alex <alexkravchenko27@gmail.com>
2022-02-16 17:11:22 +0200

    add sign clear and bckspc btns

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
 const numbers = document.querySelectorAll('.numbers');
-let out = document.querySelector('.output');
+const signs = document.querySelectorAll('.signs');
 const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+const backspace = document.querySelector('.backspace');
+let out = document.querySelector('.output');
+
+let outCount = '';
 
 numbers.forEach(elem => {
   elem.addEventListener('click', () => {
     out.innerHTML += elem.innerHTML;
+    outCount = out.innerHTML
+  })
+});
+
+signs.forEach(elem => {
+  elem.addEventListener('click', () => {
+    out.innerHTML += `${elem.innerHTML}`;
   })
 });
 
 equals.addEventListener('click', () => {
-  out.innerHTML = eval(out.innerHTML)
-})
+  out.innerHTML = eval(out.innerHTML);
+  outCount = out.innerHTML;
+});
+
+clear.addEventListener('click', () => {
+  out.innerHTML = '';
+});
+
+backspace.addEventListener('click', () => {
+  outCount = outCount.slice(0, -1);
+  out.innerHTML = outCount;
+});
